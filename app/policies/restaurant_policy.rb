ada3d39071ce9feaf831dcf6385d278356984525
sema karan <karan18sema@gmail.com>
2024-03-01 10:10:04 +0100

    update

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -8,15 +8,11 @@ class RestaurantPolicy < ApplicationPolicy
   def show?
     true
   end
-  def new?
-    create?
-  end
+
   def create?
     true
   end
-  def edit?
-    update?
-  end
+
   def update?
     record.user == user
     # record: the restaurant passed to the `authorize` method in controller
